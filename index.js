8fbfe30d39cb1742fcabfcbbd9314cd352c37e46
priscillah barasa <beverlybarasa@gmail.com>
2024-04-27 14:56:52 +0300

    domcontentload

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    var paragraph = document.getElementById("text");
+
+    // Replace the text content with "This is really cool!"
+    paragraph.textContent = "This is really cool!";
+});
+    console.log('paragraph')
